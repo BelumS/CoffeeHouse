ffdaa278a3ce3f715e1c4bcc5751f041181321c4
Mitch <Mitch@Mitch-HP>
2016-11-22 16:34:22 -0600

    abc

--- QuadChat/src/main/java/com/quadcore/chat/dao/UserDAOImpl.java
+++ QuadChat/src/main/java/com/quadcore/chat/dao/UserDAOImpl.java
@@ -14,34 +14,7 @@ import com.quadcore.chat.model.User;
  *
  */
 @Service
-public class UserDAOImpl implements UserDAO {
+public class UserDAOImpl {
 
-	/* (non-Javadoc)
-	 * @see com.quadcore.chat.dao.UserDAO#authenticate(java.lang.String, java.lang.String)
-	 */
-	@Override
-	public boolean authenticate(String username, String password) 
-	{
-		//Provide a sample password check
-		return Objects.equals(username, password);
-	}
-
-	/* (non-Javadoc)
-	 * @see com.quadcore.chat.dao.UserDAO#changePassword(com.quadcore.chat.model.User, java.lang.String)
-	 */
-	@Override
-	public void changePassword(User user, String password) {
-		// TODO Auto-generated method stub
-
-	}
-
-	/* (non-Javadoc)
-	 * @see com.quadcore.chat.dao.UserDAO#changeEmail(com.quadcore.chat.model.User, java.lang.String)
-	 */
-	@Override
-	public void changeEmail(User user, String email) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
 }
