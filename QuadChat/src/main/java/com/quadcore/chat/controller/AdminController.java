50e0f40f9c3906023ef93eab92c35506da7d2799
belum <belum@belum-NV59C>
2017-01-24 21:52:24 -0600

    last push from JTerm 16/17

--- QuadChat/src/main/java/com/quadcore/chat/controller/AdminController.java
+++ QuadChat/src/main/java/com/quadcore/chat/controller/AdminController.java
@@ -1,9 +1,16 @@
 package com.quadcore.chat.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.quadcore.chat.model.User;
 import com.quadcore.chat.service.UserService;
 
 /**
@@ -19,13 +26,34 @@ import com.quadcore.chat.service.UserService;
 @Controller
 public class AdminController {
 	
-	//@Autowired
-	//private UserService userService;
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping("/admin")
-	public String displayAdminPage()
+	public String displayAdminPage(@ModelAttribute("user") User user, Model model)
 	{
-		return "/admin";
+		model.addAttribute("user", new User());
+		return "admin";
+	}
+	
+	@PostMapping("/admin")
+	public String submitUserForm(@Valid @ModelAttribute("user") User user, BindingResult result, Model model)
+	{
+		if(result.hasErrors())
+		{
+			return "admin?error";
+		}
+		else if(userService.findByUsername(user.getUsername()))
+		{
+			model.addAttribute("message", "Username already exists. Try another username.");
+			return "admin?error";
+		}
+		else
+		{
+			userService.save(user);
+			model.addAttribute("message", "Saved user details");
+			return "results";
+		}
 	}
 
 }
